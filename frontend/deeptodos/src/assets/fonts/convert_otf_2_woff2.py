f2a71ebb439f6aa7239ad889f7f1d58fdf31846f
Li-Wend <872006164@qq.com>
2026-03-27 17:26:14 +0800

    coding format

--- frontend/deeptodos/src/assets/fonts/convert_otf_2_woff2.py
+++ frontend/deeptodos/src/assets/fonts/convert_otf_2_woff2.py
@@ -1,10 +1,10 @@
 from fontTools.ttLib import TTFont
 
 # 加载字体文件
-font = TTFont('SourceHanSansCN-Regular.otf')
+font = TTFont("SourceHanSansCN-Regular.otf")
 
 # 设置输出格式为woff
-font.flavor = 'woff2'
+font.flavor = "woff2"
 
 # 保存为woff2格式
-font.save('SourceHanSansCN-Regular.woff2')
+font.save("SourceHanSansCN-Regular.woff2")
